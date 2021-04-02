5271665bde0266c8f10f2cdd9a2b6eea7bfc081d
Shubhda123 <74812212+Shubhda123@users.noreply.github.com>
2021-04-02 09:45:12 +0530

    Add files via upload

---
+++ Bob.js
@@ -0,0 +1,29 @@
+class Bob{
+	constructor(x,y,r)
+	{
+		var options={
+			restitution:0.3,
+			friction:5,
+			density:1
+		}
+		this.x=x;
+		this.y=y;
+		this.r=r
+		this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options)
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+			var bobpos=this.body.position;		
+			push()
+			translate(bobpos.x, bobpos.y);
+			rectMode(CENTER)
+			strokeWeight(4);
+			stroke("black");
+			fill("darkblue");
+			ellipse(0,0,this.r, this.r);
+			pop()
+	}
+
+}
